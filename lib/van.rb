f7854153c88f3f04bc3c9b3bf36b93e771172c0c
andy jenkins <andygjenkins@gmail.com>
2016-09-30 08:28:50 +0100

    Fix formatting

--- lib/van.rb
+++ lib/van.rb
@@ -1,19 +1,19 @@
 class Van
 
-attr_accessor :cargo
+  attr_accessor :cargo
 
-def initialize
-  @cargo = []
-end
+  def initialize
+    @cargo = []
+  end
 
-def pick_up
-  @bikes.each do |bike|
-    if bike.broken?
-      @cargo << bike
-      @bikes.delete bike
+  def pick_up
+    @bikes.each do |bike|
+      if bike.broken?
+        @cargo << bike
+        @bikes.delete bike
+      end
     end
-              end
-end
+  end
 
 
 
